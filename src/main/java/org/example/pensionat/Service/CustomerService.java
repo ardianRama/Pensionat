fd51c57c0a2167936ff30922286628c50297fff1
SebastianV <sebbe.live@hotmail.com>
2025-05-21 10:20:04 +0200

    service klassen

---
+++ src/main/java/org/example/pensionat/Service/CustomerService.java
@@ -0,0 +1,54 @@
+package org.example.pensionat.Service;
+
+import org.example.pensionat.models.Customer;
+import org.example.pensionat.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+import java.util.Optional;
+
+// service
+@Service
+public class CustomerService {
+
+    //private static final BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
+
+
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    public boolean existsUsername(String username){
+        return customerRepository.findByUsername(username).isPresent();
+    }
+
+    public Optional<String> findByUsername(String username) {
+        return customerRepository.findByUsername(username).map(Customer::getPassword);
+    }
+
+    /*
+    public String encryptPassword(String password) {
+        return encoder.encode(password);
+    }
+
+    public boolean matchPassword(String okrypterat, String krypterat) {
+        return encoder.matches(okrypterat, krypterat);
+    }
+
+     */
+
+    public Customer registerUsername(String username, String password, String name, String email, String phoneNumber, String address) {
+        Customer c = new Customer();
+        c.setUsername(username);
+        c.setPassword(password);
+        c.setName(name);
+        c.setEmail(email);
+        c.setPhoneNumber(phoneNumber);
+        c.setAddress(address);
+        return customerRepository.save(c);
+    }
+}
