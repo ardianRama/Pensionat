ca2d6ad17b53fe27f1ea2a436027d3113261c4a8
Ardian Rama <ardian.rama@hotmail.se>
2025-05-27 11:08:40 +0200

    initial commit

--- src/main/java/org/example/pensionat/controllers/BookingController.java
+++ src/main/java/org/example/pensionat/controllers/BookingController.java
@@ -89,7 +89,7 @@ public class BookingController {
     }
 
 
-    //@RestController!, behöver göras om
+    //@RestController!, behöver göras om.
     @PutMapping("/booking/{id}/update")
     public ResponseEntity<String> updateBooking(@PathVariable Long id,
                                                 @RequestBody @Valid DetailedBookingDto bookingDto) {
